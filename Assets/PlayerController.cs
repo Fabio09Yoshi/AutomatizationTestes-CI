882b53d8c2e559ba24f66593ff7df0311f75235a
Fabio09Yoshi <fabio.yoshikawa09@gmail.com>
2025-05-22 22:19:22 -0300

    Controle de Ações - Heal e Defend

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements.Experimental;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,9 +12,15 @@ public class PlayerController : MonoBehaviour
     public int current_health = 100;
     public int maxHealth = 100;
     public int damage = 10;
+    public int numberOfPotions = 3;
     public bool attacking;
+    public bool defending;
+    public bool healing;
+    public bool hurt;
     public float maxDistance = 5f;
     public float waitTimeAttack;
+    public float waitTimeToDefend;
+    public float waitTimeToHeal;
 
     Animator anim;
     [Header("Enemy")]
@@ -22,6 +29,7 @@ public class PlayerController : MonoBehaviour
     [Header("UI")]
     public GameObject combatPanel;
     public GameObject healtBar;
+    public Button healButton;
 
 
     // Start is called before the first frame update
@@ -48,7 +56,6 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("Attack", attacking);
         StartCoroutine("WaitAttack");
         
-
     }
 
     IEnumerator WaitAttack()
@@ -60,5 +67,50 @@ public class PlayerController : MonoBehaviour
         combatPanel.SetActive(true);
         StopCoroutine("WaitAttack");
     }
+
+
+    public void Defend()
+    { 
+        defending = true;
+        anim.SetBool("Defend", defending);
+        StartCoroutine("Defending");
+    }
+
+    IEnumerator Defending()
+    {
+        yield return new WaitForSeconds(waitTimeToDefend);
+        defending = false;
+        anim.SetBool("Defend", defending);
+        combatPanel.SetActive(true);
+        StopCoroutine("waitTimeToDefend");
+    }
+
+    public void Heal()
+    {
+        if (numberOfPotions > 0)
+        {
+            healing = true;
+            numberOfPotions--;
+            anim.SetBool("Healing", healing);
+            StartCoroutine("Healing");
+        }
+     
+    }
+
+    IEnumerator Healing()
+    {       
+        yield return new WaitForSeconds(waitTimeToHeal);
+        healing = false;
+        anim.SetBool("Healing", healing);
+        combatPanel.SetActive(true);
+        if (numberOfPotions == 0)
+        {
+            healing = false;
+            healButton.image.color = Color.black;
+            healButton.interactable = false;
+        }
+        StopCoroutine("Healing");
+    }
+            
 
 }
